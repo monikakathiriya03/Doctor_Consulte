737ceca21fbd785869ebd589d1ac00a21341934a
monika kathiriya <kathiriyamonika27@gmail.com>
2024-05-09 18:17:02 +0530

    09-05-2024

---
+++ src/app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = process.env.PORT;
+const morgan = require("morgan");
+// const path = require("path");
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(port,() => {
+    async function main() {
+        await mongoose.connect(process.env.MONGO_DB_URL);
+      }
+      main()
+        .then(() => console.log("DB is Connected...✔️"))
+        .catch((err) => console.log(err.message));
+    
+      console.log(`Server start at http://localhost:${port}`);
+    });
+    
